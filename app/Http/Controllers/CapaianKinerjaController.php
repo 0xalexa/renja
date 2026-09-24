5a72af2ffcb8cc92cd43a8869e52b520789e0977
ikari321 <alexxakussuma@gmail.com>
2026-09-24 09:16:19 +0700

    fix: cegah browser cache pada stream export pdf capaian kinerja

--- app/Http/Controllers/CapaianKinerjaController.php
+++ app/Http/Controllers/CapaianKinerjaController.php
@@ -927,7 +927,10 @@ class CapaianKinerjaController extends Controller
             'isPdf' => true,
         ])->setPaper($paperSize, 'landscape');
 
-        return $pdf->stream($filename);
+        return $pdf->stream($filename)
+            ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 
     public function cetakPdf(Request $request)
